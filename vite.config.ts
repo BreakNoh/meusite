89a029cba98ccc0a99f87c92a9a3adaf74a4f5f1
BreaKnoh <brematosmene@gmail.com>
2026-04-01 10:58:52 -0300

    corrige fundo no build

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
     plugins: [svelte()],
     base: "/",
+    build: {
+        assetsDir: "assets",
+        emptyOutDir: true,
+    },
+    publicDir: "public",
 });
